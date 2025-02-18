e7315528f97051157f1d75a002dbdea8856056bb
Nestor Allende <74310266+Nestor20193767@users.noreply.github.com>
2025-02-18 11:05:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import faiss
 import numpy as np
 from sentence_transformers import SentenceTransformer
 import streamlit as st
-from google import genai
+import google.generativeai as genai
 
 # 🚀 Configure Gemini API
 api_key = "AIzaSyAgDe959MVEgOz7Z5WtXgIIRXY-5DA54co"
